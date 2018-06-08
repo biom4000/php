02b215a51a8bd63f107d1e170272f627a0c53812
Andywa <biom4000@yahoo.com.tw>
2018-06-08 23:04:08 +0800

    0608

--- php/connect_mysql.php
+++ php/connect_mysql.php
@@ -7,8 +7,8 @@
  */
 
 $db_server  = "localhost";
-$db_user = "bion4000";
-$db_passwd  = "ga9871109kppq";
+$db_user = "root";
+$db_passwd  = "1sopqjr";
 $db_name  = "tolife";
 
 $con=mysqli_connect($db_server,$db_user,$db_passwd ,$db_name );
@@ -18,4 +18,3 @@ if (mysqli_connect_errno())
 {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-echo "ok";
--- php/index.php
+++ php/index.php
@@ -3,5 +3,35 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $id = $_POST['id'];
     $pwd = $_POST['password'];
 
-    include 'connect_mysql.php';
+
+
 }
+
+$db_server  = "localhost";
+$db_user = "root";
+$db_passwd  = "1sopqjr";
+$db_name  = "tolife";
+
+$con=mysqli_connect($db_server,$db_user,$db_passwd ,$db_name );
+mysqli_set_charset($con,"utf8");
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql = "SELECT * FROM register WHERE ID='bion4000'";
+$result = mysqli_query($con, $sql);
+
+
+if(mysqli_num_rows($result) > 0){
+    while ($row = mysqli_fetch_assoc($result)){
+        echo $row['ID'].", ".$row['Password']."<br>";
+
+    }
+}
+else{
+    echo "0 results";
+}
+
+mysqli_close($con);
