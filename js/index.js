b872901b6e61af4daef38871dd8a561bdb0971d2
Andywa <biom4000@yahoo.com.tw>
2018-06-10 17:53:41 +0800

    0610

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
-function tohome() {
-    window.open("./home.html","_self");
-}
-function tologin() {
-    window.open("./login.html","_self");
+function showHint(str1, str2) {
+
+
+    if( (str1.length == 0) || (str2.length == 0) ){
+        document.getElementById("error_showbox").innerHTML = "**ID or Password is null**";
+    }
+    else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                if(this.responseText == 'ok'){
+                    document.location.href="../html/home.html";
+                }
+                else{
+                    document.getElementById("error_showbox").innerHTML = this.responseText;
+                }
+            }
+        }
+        xmlhttp.open("POST", "../php/index.php", true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.send("id="+str1+"&pwd="+str2);
+
+    }
 }
--- php/index.php
+++ php/index.php
@@ -1,37 +1,36 @@
 <?php
+
+session_start();
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $id = $_POST['id'];
-    $pwd = $_POST['password'];
+    $_SESSION['id'] = $_POST['id'];
+    $_SESSION['pwd'] = $_POST['pwd'];
 
+    $db_server  = "localhost";
+    $db_user = "root";
+    $db_passwd  = "1sopqjr";
+    $db_name  = "tolife";
 
-
-}
-
-$db_server  = "localhost";
-$db_user = "root";
-$db_passwd  = "1sopqjr";
-$db_name  = "tolife";
-
-$con=mysqli_connect($db_server,$db_user,$db_passwd ,$db_name );
-mysqli_set_charset($con,"utf8");
+    $con=mysqli_connect($db_server,$db_user,$db_passwd ,$db_name );
+    mysqli_set_charset($con,"utf8");
 // Check connection
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-$sql = "SELECT * FROM register WHERE ID='bion4000'";
-$result = mysqli_query($con, $sql);
-
-
-if(mysqli_num_rows($result) > 0){
-    while ($row = mysqli_fetch_assoc($result)){
-        echo $row['ID'].", ".$row['Password']."<br>";
-
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-}
-else{
-    echo "0 results";
-}
 
-mysqli_close($con);
+    $sql = "SELECT * FROM register WHERE ID='".$_SESSION['id']."' AND Password='".$_SESSION['pwd']."'";
+    //echo $sql;
+    $result = mysqli_query($con, $sql);
+
+    if(mysqli_num_rows($result) > 0){
+        echo 'ok';
+    }
+    else{
+        unset($_SESSION['id']);
+        unset($_SESSION['pwd']);
+        echo '**ID or Password is not**';
+    }
+
+    mysqli_close($con);
+
+}
